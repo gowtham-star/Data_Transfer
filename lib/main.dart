c2b30a70fefba5fda080e4e090dfa482bddbf5a3
gowtham-star <gowthamtnvs@yahoo.com>
2023-09-03 12:06:44 -0400

    Added code to recieve data

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main(){
-  runApp(Myapp());
+void main() {
+  runApp(MyApp());
 }
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text('Hello World'),
-        ),
+      home: WifiApp(),
+    );
+  }
+}
 
+class WifiApp extends StatefulWidget {
+  @override
+  _WifiAppState createState() => _WifiAppState();
+}
+
+class _WifiAppState extends State<WifiApp> {
+  String collectedData = "No data yet";
+
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('http://raspberrypi-ip-address:5000/api/data'));
+    if (response.statusCode == 200) {
+      setState(() {
+        collectedData = json.decode(response.body).toString();
+      });
+    } else {
+      setState(() {
+        collectedData = "Error fetching data";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wi-Fi App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Collected Data:', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text(collectedData, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: fetchData,
+              child: Text('Fetch Data'),
+            ),
+          ],
+        ),
       ),
     );
   }
