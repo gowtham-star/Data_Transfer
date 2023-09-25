1c8def16f09955b418a8fc7ee8aee1c9b36be91a
gowtham-star <gowthamtnvs@yahoo.com>
2023-09-25 17:32:45 -0400

    Added multiple attributes
    In flask app

--- flask_app/ExposeAPI.py
+++ flask_app/ExposeAPI.py
@@ -1,23 +1,21 @@
 from flask import Flask, jsonify
 import random
 
+#Flask App Initalisation
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Raspberry Pi!'
 
 def generate_random_temperature_celsius():
     # Generate a random temperature between -20°C and 40°C
     temperature = random.uniform(-20, 40)
     return temperature
 
-@app.route('/api/data')
+@app.route('/')
 def get_data():
     data = {
         'temperature': generate_random_temperature_celsius()
+        'random': random.uniform(0, 10000)
     }
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5500)
